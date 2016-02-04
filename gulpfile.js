e2087263a8b71d753b23c6cc59a29346f9441884
Niels Vermaut <nielsvermaut@gmail.com>
2016-02-04 11:59:24 +0100

    Updated gulp flow

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var runSequence = require('run-sequence');
 var path = require('path');
+var babel = require('gulp-babel');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var reactify = require('reactify');
@@ -27,22 +28,15 @@ gulp.task('build', function(){
  * Minifies and concatenates (React) JavaScript to one file.
  * This file will be written to web/dist/js/napoleon-react.js
  */
-gulp.task('concatReact', function(){
-    util.log('Minifying React JS');
+gulp.task('build', function(){
+    return gulp.src('src/ComponentSpawner.react.js')
+        .pipe(babel({
+            presets: ['es2015','stage-0','react']
+        }))
+        .pipe(gulp.dest('dist'));
+});
 
-    var bundler = browserify({
-        entries: ["src/ComponentSpawner.react.js"],
-        debug: true,
-        cache: {}, packageCache: {}, fullPaths: true
-    });
-
-    bundler
-        .transform(babelify, {presets: ['es2015', 'react', 'stage-0'], plugins: ['add-module-exports']})
-        .bundle()
-        .pipe(source('ComponentSpawner.js'))
-        .pipe(gulp.dest('dist/'))
-    ;
-
+gulp.task('buildTest', function(){
     var bundlerTestBed = browserify({
         entries: ["example/js/src/bootstrap.js"],
         debug: true,
@@ -54,5 +48,5 @@ gulp.task('concatReact', function(){
         .bundle()
         .pipe(source('ComponentFactory.js'))
         .pipe(gulp.dest('example/js/dist/'))
-    ;
+        ;
 });
